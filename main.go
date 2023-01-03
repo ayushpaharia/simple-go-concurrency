b1c3a531f2e0fd07a035f69ac7de880b0174b896
ayushpaharia <ayush.paharia.18@gmail.com>
2023-01-03 21:10:51 +0530

    feat: with WaitGroup

--- main.go
+++ main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	started := time.Now()
 	needs := []string{"food", "water", "shelter", "company"}
+	var wg sync.WaitGroup
+	wg.Add(len(needs))
 	for _, f := range needs {
 		go func(f string) {
 			get(f)
+			wg.Done()
 		}(f)
 	}
+	wg.Wait()
 	fmt.Printf("Got in %v\n", time.Since(started))
 }
 
