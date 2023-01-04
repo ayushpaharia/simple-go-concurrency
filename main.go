e20db79823166fd478d9c59eedab32a1fdaa77a2
ayushpaharia <ayush.paharia.18@gmail.com>
2023-01-04 14:10:01 +0530

    feat: with Channel

--- main.go
+++ main.go
@@ -7,8 +7,29 @@ import (
 )
 
 func main() {
+
+	withWaitGroup([]string{"food", "water", "shelter", "company"})
+	fmt.Println("")
+	withChannel([]string{"gas", "fulfillment", "purpose"})
+}
+
+func withChannel(needs []string) {
 	started := time.Now()
-	needs := []string{"food", "water", "shelter", "company"}
+	ch := make(chan string)
+	for _, f := range needs {
+		go func(f string) {
+			get(f)
+			ch <- f
+		}(f)
+	}
+	for range needs {
+		<-ch
+	}
+	fmt.Printf("Got in %v\n", time.Since(started))
+}
+
+func withWaitGroup(needs []string) {
+	started := time.Now()
 	var wg sync.WaitGroup
 	wg.Add(len(needs))
 	for _, f := range needs {
@@ -23,7 +44,7 @@ func main() {
 
 func get(need string) {
 	fmt.Printf("Getting %s...\n", need)
-	time.Sleep(2 * time.Second)
-	fmt.Printf("Got %s\n", need)
+	time.Sleep(1 * time.Second)
+	fmt.Printf("Got %s", need)
 	fmt.Println("")
 }
